a8c2d3d3ff461e7615ae603dc1b28213ee4d33be
Mhmd-Git47 <mohammadrawass12@gmail.com>
2025-06-22 00:52:02 +0300

    Updated postgre info

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on localhost: ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
