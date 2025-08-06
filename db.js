b4d95759fa8a84268af8af4135bfd7fc2082755f
Mhmd-Git47 <mohammadrawass12@gmail.com>
2025-08-06 10:40:00 +0300

    Updated pool and ssl

--- db.js
+++ db.js
@@ -5,9 +5,9 @@ const pool = new Pool({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD || "",
-  port: process.env.PGPORT,
+  port: Number(process.env.PGPORT),
   ssl: {
-    rejectUnauthorized: false, 
+    rejectUnauthorized: false,
   },
 });
 
